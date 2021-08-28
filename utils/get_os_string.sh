cadeb8a9ddf2cc49f1cdff6b379616d60028fa41
zhangzheng <zzgigi2003@163.com>
2021-08-28 18:11:24 +0800

    Create get_os_string.sh

---
+++ utils/get_os_string.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Operating System (pass-test): 
+#
+# 
+# 1. Ubuntu 20.04.2 LTS
+# 2. Red Hat Enterprise Linux 8.4 (Ootpa)
+# 3. Amazon Linux 2
+#
+
+if hostnamectl | grep "Amazon" >/dev/null 2>&1; then
+    echo "amazon"
+    exit
+fi
+
+if hostnamectl | grep "Ubuntu" >/dev/null 2>&1; then
+    echo "ubuntu"
+    exit
+fi
+
+if hostnamectl | grep "Red Hat" >/dev/null 2>&1; then
+    echo "redhat"
+    exit
+fi
+
+echo "linux"
