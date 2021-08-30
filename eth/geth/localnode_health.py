14ea9c7c01730b2a917472b087b3692363b2079a
zhangzheng <zzgigi2003@163.com>
2021-08-30 12:44:27 +0800

    Update localnode_health.py

--- eth/geth/localnode_health.py
+++ eth/geth/localnode_health.py
@@ -1,5 +1,13 @@
 #/usr/bin/env python
 
+#
+# requires your to be set the environment variable before running
+#
+# Ding Bot Access Token:
+#
+#     export DING_BOT_ACCESS_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+
 from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
