9c9e227ba6f48861b345355c99be2ebb5efca804
zhangzheng <zzgigi2003@163.com>
2021-08-30 12:21:19 +0800

    Update and rename health.py to localnode_health.py

--- eth/geth/localnode_health.py
+++ eth/geth/localnode_health.py
@@ -5,4 +5,5 @@ from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
 
 if not w3.isConnected():
-  print("geth is not connect")
+  # send msg to dingding
+  print("[eth] [local node] geth is not available")
